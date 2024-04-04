030399fea4117805329517239e0d5ca986fa7f20
dmoskowitzbooth <165198611+dmoskowitzbooth@users.noreply.github.com>
2024-04-04 15:12:55 +0000

    thinkfast

---
+++ thinkfast.rb
@@ -0,0 +1,28 @@
+unpredictable_inputs = [
+  "Hello!",
+  Time.now,
+  rand(100),
+  :GOODBYE,
+  nil,
+  true,
+  false,
+  { :city => "Chicago", :state => "IL", :zip => 60654 }
+]
+
+some_random_input = unpredictable_inputs.sample
+think=some_random_input.class
+
+if
+  think=="String"
+  pp some_random_input.downcase
+elsif think=="Time"
+  pp "Time"
+elsif think=="Integer"
+  if
+    some_random_input.even?
+    pp some_random_input + "is even"
+  elsif
+    some_random_input.odd?
+    pp some_random_input + "is odd"
+  end
+end
